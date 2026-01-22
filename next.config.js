ad1b2facdbc093d5466d8b03165b6d5c4dde9fe9
Elezar <dinagat.elezar887@gmail.com>
2026-01-22 17:21:03 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/timbersouq',
-  assetPrefix: '/timbersouq/',
   images: {
     unoptimized: true,
   },
